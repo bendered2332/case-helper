84d9cbc2e1ccc91d98771ecbe8043a059ffdda3d
Edward Bender <59852058+bendered2332@users.noreply.github.com>
2023-02-13 13:06:07 -0500

    Added email and google signin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './component/verify-email/verify-email.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'case-helper-9d96e',
+    appId: '1:1006207073204:web:0e4afd30acc7f22521d0e1',
+    storageBucket: 'case-helper-9d96e.appspot.com',
+    apiKey: 'AIzaSyAG3cNOddp11ZIqPmmD_TQ3nyQe04wnA7Y',
+    authDomain: 'case-helper-9d96e.firebaseapp.com',
+    messagingSenderId: '1006207073204',
+  },
+    
+}
